80c3cf7f51a60f4b1e44d08d356d9f5ee407c4f1
Thomas Nguyen-Huu <thomas.qnh@gmail.com>
2024-10-31 16:20:24 +0100

    Update main JS

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import { $, $$ } from 'select-dom';
 import 'scripts/init';
 import 'styles/main.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (isTouchDevice()) removeClass($('body'), 'no-touch');
-  splitIntoWords($$('.js-text-beautifier'));
-  splitIntoLetters($$('.js-text-beautifier a'));
-  initScrollSnap($('#sections-wrapper'));
-});
+if (isTouchDevice()) removeClass($('body'), 'no-touch');
+splitIntoWords($$('.js-text-beautifier'));
+splitIntoLetters($$('.js-text-beautifier a'));
+initScrollSnap($('#sections-wrapper'));
