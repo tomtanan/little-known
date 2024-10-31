bb8928beeb8b8626534c930fb2116508dfa021c2
Thomas Nguyen-Huu <thomas.qnh@gmail.com>
2024-10-31 16:11:00 +0100

    Update styles to deactivate hover effects on touch devices

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { splitIntoWords, splitIntoLetters } from 'utils/helpers';
+import { splitIntoWords, splitIntoLetters, isTouchDevice, removeClass } from 'utils/helpers';
 import { initScrollSnap } from 'components/ScrollSnapController.js';
 import { $, $$ } from 'select-dom';
 import 'scripts/init';
 import 'styles/main.css';
 
-splitIntoWords($$('.js-text-beautifier'));
-splitIntoLetters($$('.js-text-beautifier a'));
-initScrollSnap($('#sections-wrapper'));
+document.addEventListener('DOMContentLoaded', () => {
+  if (isTouchDevice()) removeClass($('body'), 'no-touch');
+  splitIntoWords($$('.js-text-beautifier'));
+  splitIntoLetters($$('.js-text-beautifier a'));
+  initScrollSnap($('#sections-wrapper'));
+});
